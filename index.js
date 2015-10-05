c27506268257cb7d140ab4bf4c995022cde09ffa
Blake Brown <blabrown@ca-to-blabrown-m.us.int.genesyslab.com>
2015-10-05 15:36:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
